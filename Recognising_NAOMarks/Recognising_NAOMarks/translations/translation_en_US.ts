1902bd80443d5ea16e473e49eedee789aa7a3851
Lilly Piri <lilly@lillypiri.com>
2017-07-05 14:32:43 +1000

    fixing choregraphe error

---
+++ Recognising_NAOMarks/Recognising_NAOMarks/translations/translation_en_US.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That is Naomark 64</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That is Naomark 64</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That is naomark 68</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That is naomark 68</translation>
+        </message>
+    </context>
+</TS>
